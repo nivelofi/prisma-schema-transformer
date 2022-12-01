0edcf474dca7bb6e9f177f761786b84ede737fa9
Evan Marcey <evanmarcey@gmail.com>
2022-12-01 15:57:52 -0500

    Removing debug shit

--- invoke.ts
+++ invoke.ts
@@ -6,36 +6,31 @@ import * as fs from 'fs';
 import {getDMMF, formatSchema} from '@prisma/sdk';
 import {dmmfModelsdeserializer, Model, dmmfModelTransformer} from './src';
 
-const schemaName = 'schema';
+const schemaName = 'private';
 const outputSchemaName = `${schemaName}.actual`;
 
-const schemaPath = `./prisma/${schemaName}.prisma`;
-const outputSchemaPath = `./prisma/${outputSchemaName}.prisma`;
+const schemaPath = `./fixtures/${schemaName}.prisma`;
+const outputSchemaPath = `./fixtures/${outputSchemaName}.prisma`;
 
 (async function () {
-  console.log(`1`)
-	// Format input schema file
-	const formatedSchemaString = await formatSchema({schemaPath});
-  console.log(`2`)
-	fs.writeFileSync(schemaPath, formatedSchemaString);
-  console.log(`3`)
-	const schema = fs.readFileSync(schemaPath, 'utf-8');
-  console.log(`4`)
-	const dmmf = await getDMMF({datamodel: schema});
-  console.log(`5`)
-	fs.writeFileSync(`./${schemaName}-dmmf.json`, JSON.stringify(dmmf, null, 2));
+  // Format input schema file
+  const formatedSchemaString = await formatSchema({schemaPath});
+  fs.writeFileSync(schemaPath, formatedSchemaString);
+  const schema = fs.readFileSync(schemaPath, 'utf-8');
+  const dmmf = await getDMMF({datamodel: schema});
+  fs.writeFileSync(`./${schemaName}-dmmf.json`, JSON.stringify(dmmf, null, 2));
 
-	// Transform to camelcase
-	const models = dmmf.datamodel.models as Model[];
-	const transformedModels = dmmfModelTransformer(models);
+  // Transform to camelcase
+  const models = dmmf.datamodel.models as Model[];
+  const transformedModels = dmmfModelTransformer(models);
 
-	// Deserialize models
-	const outputSchema = await dmmfModelsdeserializer(transformedModels);
-	fs.writeFileSync(outputSchemaPath, outputSchema);
-	const formatedOutputSchema = await formatSchema({schemaPath: outputSchemaPath});
-	fs.writeFileSync(outputSchemaPath, formatedOutputSchema);
+  // Deserialize models
+  const outputSchema = await dmmfModelsdeserializer(transformedModels);
+  fs.writeFileSync(outputSchemaPath, outputSchema);
+  const formatedOutputSchema = await formatSchema({schemaPath: outputSchemaPath});
+  fs.writeFileSync(outputSchemaPath, formatedOutputSchema);
 
-	// Validate
-	const outputDmmf = await getDMMF({datamodel: outputSchema});
-	fs.writeFileSync(`./${schemaName}-dmmf.actual.json`, JSON.stringify(outputDmmf, null, 2));
+  // Validate
+  const outputDmmf = await getDMMF({datamodel: outputSchema});
+  fs.writeFileSync(`./${schemaName}-dmmf.actual.json`, JSON.stringify(outputDmmf, null, 2));
 })();
